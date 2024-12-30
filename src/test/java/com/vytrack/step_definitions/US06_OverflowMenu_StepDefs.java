bf3a231e40d4dd1b1e6e98ae0f4d12a355e0d0e5
id777 <id777.doni@gmail.com>
2024-12-30 17:40:12 -0600

    updating US06

---
+++ src/test/java/com/vytrack/step_definitions/US06_OverflowMenu_StepDefs.java
@@ -0,0 +1,23 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US06_OverflowMenu_StepDefs {
+    @When("user hover the mouse over the three dots")
+    public void user_hover_the_mouse_over_the_three_dots() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user can see folowing options displayed")
+    public void user_can_see_folowing_options_displayed(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- src/test/resources/features/US06_OverflowMenu_ID.feature
+++ src/test/resources/features/US06_OverflowMenu_ID.feature
@@ -4,7 +4,7 @@ Feature: Users should be able to see content of the OverflowMenu
   Scenario Outline: Verify that users see “view, edit, delete” options when they hover the mouse
   over the overflow menu
     Given the user logged in as "<userType>"
-    When user navigates to "Fleet" to "Vehicles" page
+    When user is on the "Fleet" on "Vehicles" page
     When user hover the mouse over the three dots
     Then user can see folowing options displayed
       | View   |
